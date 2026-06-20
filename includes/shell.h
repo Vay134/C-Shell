85ec5029ca3bec8aa7e572d676cad7f49eed06e8
Vay134 <71159266+Vay134@users.noreply.github.com>
2026-06-20 16:14:39 +0000

    feat(builtin commands): Created template functions for builtin command implementation

--- includes/shell.h
+++ includes/shell.h
@@ -15,4 +15,19 @@
 void type_prompt();
 void read_command(char **cmd);
 
+extern const char *builtin_commands[];
+
+/*
+Handler of each shell builtin function
+*/
+int shell_cd(char **args);
+int shell_help(char **args);
+int shell_exit(char **args);
+int shell_usage(char **args);
+int list_env(char **args);
+int set_env_var(char **args);
+int unset_env_var(char **args);
+
+extern int (*builtin_command_func[])(char **);
+
 #endif
